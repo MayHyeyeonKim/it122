bdec1b6202955ffdc4f19bd154236cc4dcd81223
MayHyeyeonKim <hkim7963@gmail.com>
2023-04-11 02:04:38 +0000

    updated week2

--- week2/data.js
+++ week2/data.js
@@ -1,7 +1,17 @@
-const data = [
+const items = [
   {id: 1, name: 'Americano', price: 3.95, calorie: 15},
   {id: 2, name: 'Pink Drink', price: 5.45, calorie: 140},
-  {id: 3, name: 'Hot Chocolate', price: 4.91, calorie: 370}
+  {id: 3, name: 'Hot Chocolate', price: 4.91, calorie: 370},
+  {id: 4, name: 'Mocha Cookie Crumble Frappuccino', price: 5.95, calorie: 480},
+  {id: 5, name: 'Caramel Frappuccino', price: 6.25, calorie: 380}
 ];
 
-module.exports = data;
+const getAll = () => {
+  return items;
+};
+
+const getItem = (id) => {
+  return items.find((item) => item.id === id);
+};
+
+module.exports = { getAll, getItem };
--- week2/index.js
+++ week2/index.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const app = express();
 const data = require('./data');
-app.set('view engine', 'ejs');
 
-app.use(express.json()); // JSON 형식의 요청 바디를 파싱
-app.use(express.urlencoded({ extended: false })); // URL-encoded 형식의 요청 바디를 파싱
-app.use(express.static('public')); // 정적 파일을 서비스하기 위한 폴더 지정
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  // res.send('Hello! This is week2 page');
   res.redirect('/home');
 });
+
 app.get('/home', (req, res) => {
-  res.render('home', { data: data });
+  const items = data.getAll();
+  res.render('home', { data: items });
 });
+
 app.get('/detail', (req, res) => {
   const id = req.query.id;
-  const item = data.find(item => item.id == id);
+  const item = data.getItem(id);
   
   if (item) {
-    const productId = req.query.id;
-    const product = data.filter(product => product.id == productId)[0];
-    res.render('detail', { item: item, product: product });
+    res.render('detail', { item: item });
   } else {
     res.status(404).send('Not Found');
   }
